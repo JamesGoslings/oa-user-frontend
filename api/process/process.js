17f8539348f7aa507a1a05ba1a295fa603d86cb3
JamesGosing <3248175240@qq.com>
2024-05-15 00:19:10 +0800

    feat: 完成审批模板显示页面

--- api/process/process.js
+++ api/process/process.js
@@ -2,9 +2,17 @@ import { request } from "/utils/common_utils/request"
 
 const PROCESS_TYPE_URL = '/admin/process/processType/'
 
+const PROCESS_TEMPLETE_URL = '/admin/process/processTemplate/'
+
 // 获取所有的审批类型
 export function getAllProcessType(){
 	return request({
 		url: PROCESS_TYPE_URL + 'all',
+	})
+}
+// 获取所有的模板
+export function getAllProcessTemplate(){
+	return request({
+		url: PROCESS_TEMPLETE_URL + 'all'
 	})
 }
--- pages/processFun/processFun.vue
+++ pages/processFun/processFun.vue
@@ -1,26 +1,80 @@
 <template>
 	<layout :lay="myLay"></layout>
 	<view class="processAll">
-		<div class="type funTitle">人事</div>
+		<view v-for="(type) in processTypeAll">
+			<view class="type funTitle">{{type.name}}</view>
+			<view v-for="(tmp) in processTemplateAll">
+				<uni-card padding="20rpx 0" v-if="tmp.processTypeId === type.id">
+						<view class="tmpOne">
+							<view class="icoBox" :style="getIcoSty(type.id)">
+								<view class="myIconfont ico" v-html="tmp.iconUrl"></view>
+							</view>
+							<view class="tmpMsg">{{tmp.name}}</view>
+						</view>
+				</uni-card>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script setup>
-import { getAllProcessType } from '../../api/process/processType';
+import { getAllProcessType,getAllProcessTemplate } from '../../api/process/process';
+import { colors } from '../../utils/common_utils/staticData';
 // 用于设定顶头信息
 let myLay = ref({title: '审批页面',mainColor:"#fff",btnColor:"#F5F5F5"})
 
+// 用于设置当前图标背景色
+function getIcoSty(id){
+	id %= colors.length
+	return {background: colors[id]}
+}
+
+// 存所有的类型
+let processTypeAll = ref([])
+// 获取类型
+const getTypeAll = async()=>{
+	let {data:{data}} = await getAllProcessType()
+	processTypeAll.value = data
+}
+// 获取所有模板
+let processTemplateAll = ref([])
+const getTemplateAll = async()=>{
+	let {data:{data}} = await getAllProcessTemplate()
+	processTemplateAll.value = data
+}
+onShow(()=>{
+	getTypeAll()
+	getTemplateAll()
+})
 </script>
 
 <style lang="scss" scoped>
 @import '@/common/myStyles/listSize.scss';
 @import '@/common/myStyles/globalPage.scss';
 @import '@/common/myStyles/commonStyles.scss';
+@import url('@/static/fontTotal/iconfont.css');
 
 .processAll{
 	padding: 0 2vw;
 	.type{
 		margin: 1vh 0;
 	}
+		.tmpOne{
+			@include flex-box;
+			justify-content: left;
+			.icoBox{
+				@include flex-box;
+				// padding: 2vh 2.7vw;
+				width: 100rpx;
+				height: 100rpx;
+				border-radius: 50%;
+				background: rgb(48,207,149);
+				.ico{
+					font-size: 50rpx;
+					color: #FFF;
+				}
+			}
+	}
+	
 }
 </style>
